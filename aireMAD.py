c24969d8c4c46eab450eb1b3922c7c07249bcea1
Ignacio Lis <ilismal@users.noreply.github.com>
2017-11-28 19:39:13 +0100

    Create aireMAD.py

---
+++ aireMAD.py
@@ -0,0 +1,30 @@
+import requests
+import csv
+import datetime
+
+url='http://datos.madrid.es/egob/catalogo/212531-10515086-calidad-aire-tiempo-real.txt'
+
+r=requests.get(url)
+respuesta=r.content
+horaAnterior = str((datetime.datetime.now() - datetime.timedelta(hours=1)).hour)
+lecturaHora = "H" + horaAnterior
+comprobacionHora = "V" + horaAnterior
+datos = csv.DictReader(respuesta.splitlines(),delimiter=';')
+for row in datos:
+    if (row['ESTACION'] == "38") & (row['MAGNITUD'] == "8"):
+        if row[comprobacionHora] == "V":
+            estadoLectura = "valida"
+        else:
+            estadoLectura = "erronea"
+        print "Ultima lectura a las " + horaAnterior + "h - Lectura " + estadoLectura
+        valorLectura = int(row[lecturaHora])
+        if (180 < valorLectura < 200):
+            estado = "PREAVISO"
+        elif (200 < valorLectura < 400):
+            estado = "AVISO"
+        elif (valorLectura > 400):
+            estado = "ALERTA"
+        else:
+            estado = "NORMAL"
+        print "Nivel de N02 en Cuatro Caminos: " + str(valorLectura)
+        print "Estado de la estacion: " + estado
